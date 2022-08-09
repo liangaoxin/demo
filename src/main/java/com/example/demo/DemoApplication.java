ef10e60a406680df6eb6e2c5871888ce2006fa5f
aoxinLiang <377183577@QQ.com>
2022-08-09 22:49:12 +0800

    第四次提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         //第三次
+        //第四次 hot-fix
         SpringApplication.run(DemoApplication.class, args);
     }
 
